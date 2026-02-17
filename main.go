31b4077478547c7c134bb518f97b7b0af87f0e05
saberd <mail@saberd.com>
2026-02-17 23:03:25 +0000

    simplify ip4 scanning

--- main.go
+++ main.go
@@ -31,14 +31,13 @@ func main() {
 			os.Exit(1)
 		}
 	} else {
-		// Get real network interfaces
-		sysIfaces, err := net.Interfaces()
+		// Get real network interfaces.
+		var err error
+		ifaces, addrsByIface, err = scan.DiscoverInterfaces()
 		if err != nil {
 			fmt.Println("Error getting network interfaces:", err)
 			os.Exit(1)
 		}
-
-		ifaces, addrsByIface = getRealInterfaces(sysIfaces)
 	}
 
 	if len(ifaces) == 0 {
@@ -60,7 +59,7 @@ func main() {
 		scanner:      scanner,
 		cursor:       0,
 		progress: progress.New(
-			progress.WithScaledGradient("#FFD700", "#B8B000"), // Bright yellow to muted gold
+			progress.WithScaledGradient("#FFD700", "#B8B000"),
 		),
 		selected: false,
 	}
@@ -73,50 +72,4 @@ func main() {
 		fmt.Printf("Error starting the program: %v", err)
 		os.Exit(1)
 	}
-}
-
-// getRealInterfaces extracts valid IPv4 network interfaces
-func getRealInterfaces(sysIfaces []net.Interface) ([]net.Interface, map[string][]net.Addr) {
-	ifaces := make([]net.Interface, 0, len(sysIfaces))
-	addrsByIface := make(map[string][]net.Addr, len(sysIfaces))
-	for _, iface := range sysIfaces {
-		// Skip loopback interfaces
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue
-		}
-
-		// Skip interfaces that are down
-		if iface.Flags&net.FlagUp == 0 {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			continue
-		}
-
-		// Skip interfaces with no addresses
-		if len(addrs) == 0 {
-			continue
-		}
-
-		// Check for at least one IPv4 address
-		hasIPv4 := false
-		for _, addr := range addrs {
-			if ipnet, ok := addr.(*net.IPNet); ok {
-				if ipnet.IP.To4() != nil {
-					hasIPv4 = true
-					break
-				}
-			}
-		}
-
-		if !hasIPv4 {
-			continue
-		}
-
-		ifaces = append(ifaces, iface)
-		addrsByIface[iface.Name] = addrs
-	}
-	return ifaces, addrsByIface
 }
